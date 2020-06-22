c0d61071471360b6a4f82e6d0d02bb6fd79108bf
aydar-it <svvedde@gmail.com>
2020-06-22 14:31:14 +0300

    hw3

---
+++ src/main/java/com/geekbrains/java/lesson3/homework/barrier/Wall.java
@@ -0,0 +1,13 @@
+package com.geekbrains.java.lesson3.homework.barrier;
+
+public class Wall {
+    private int height;
+
+    public Wall(int height) {
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
---
+++ src/main/java/com/geekbrains/java/lesson3/homework/runner/Jumper.java
@@ -0,0 +1,7 @@
+package com.geekbrains.java.lesson3.homework.runner;
+
+import com.geekbrains.java.lesson3.homework.barrier.Wall;
+
+public interface Jumper {
+    boolean jump(Wall wall);
+}
