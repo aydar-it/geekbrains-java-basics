ecbf8675f53949d57f71d66c1af30de0ce137b78
aydar-it <svvedde@gmail.com>
2020-06-30 13:56:49 +0300

    hw6

---
+++ src/main/java/com/geekbrains/java/lesson6/task1/TaskOne.java
@@ -0,0 +1,32 @@
+package com.geekbrains.java.lesson6.task1;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+public class TaskOne {
+    public static void main(String[] args) {
+        String[] strings = {"one", "two", "two", "three", "three", "three", "four", "four", "four", "four"};
+        printOnlyOrig(strings);
+        printCountOfArrayElements(strings);
+    }
+
+    private static void printCountOfArrayElements(String[] strings) {
+        Map<String, Integer> counter = new HashMap<>();
+        for (String str : strings) {
+            Integer value = counter.get(str);
+            if (value == null) {
+                value = 1;
+            } else {
+                value++;
+            }
+            counter.put(str, value);
+        }
+        System.out.println(counter);
+    }
+
+    public static void printOnlyOrig(String[] strings) {
+        System.out.println(new HashSet<>(Arrays.asList(strings)));
+    }
+}
